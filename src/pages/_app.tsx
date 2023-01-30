42af18d4709d35242d880fef1c70e498aca3cce8
manueldembo <madembo18@gmail.com>
2023-01-30 18:16:22 +0100

    @next: configuração do header da aplicação-v1.0.0

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,19 @@
 import { AppProps } from "next/app";
 import { globalStyle } from "../styles/global";
+import { Container, Header } from "../styles/pages/App";
+
+import logoImg from '../assets/logo.svg'
 
 globalStyle()
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Container>
+      <Header>
+        <img src={logoImg.src} alt=""/>
+      </Header>
+
+      <Component {...pageProps} />
+    </Container>
+  )
 }
---
+++ src/styles/pages/app.ts
@@ -0,0 +1,16 @@
+import { styled } from "..";
+
+export const Container = styled('div', {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    minHeight: '100vh',
+})
+
+export const Header = styled('header', {
+    padding: '2rem',
+    margin: '0 auto',
+    width: '100%',
+    maxWidth: 1180,
+})
