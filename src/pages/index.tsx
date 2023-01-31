43ee8a4d179b7f59ec32684740baf4eb4edba7d2
manueldembo <madembo18@gmail.com>
2023-01-31 23:12:43 +0100

    @next: prevenindo comportamento padrao do fetch para false,  aprimorando a performace do projeto-v1.0.0

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -35,6 +35,7 @@ export default function Home({ products }: HomeProps) {
               href={`/product/${product.id}`} 
               key={product.id}
               passHref legacyBehavior
+              prefetch={false}
             >
               <Product className="keen-slider__slide" >
                 <Image src={product.imageUrl} width={520} height={480} alt=""/>
